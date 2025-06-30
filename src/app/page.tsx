615412853e05133f33208f67a0359314c550f46c
Icesuza <hengliza81@gmail.com>
2025-06-30 11:43:27 +0700

    third commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ const ubuntu = Ubuntu_Mono({
 export default function Home() {
     return (
         <main>
-            <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"><UserCard/></section>
+            <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"><UserCard/></section>
             <Image width={200} height={300} src="/image/image.png" alt="" unoptimized />
             <p className={`${ubuntu.variable} antialiased`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ipsum, aliquam quibusdam reprehenderit, illum atque tempore culpa corporis, similique id fugiat aspernatur deleniti eligendi. Dignissimos perspiciatis odio eum eos temporibus? សួស្ដី</p>
         </main>
